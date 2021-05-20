6c3cd03726da06159c6b8792411da357dd11dd30
lishimeng <316527907@qq.com>
2021-05-20 18:34:22 +0800

    task monitor

---
+++ internal/messager/task/task.ws.monitor.go
@@ -0,0 +1,97 @@
+package task
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/kataras/iris/v12/websocket"
+	"github.com/lishimeng/go-log"
+	"sync"
+)
+
+type MonitorData struct {
+	TaskId            int    `json:"taskId,omitempty"`
+	MessageId         int    `json:"messageId,omitempty"`
+	MessageCategory   int    `json:"messageCategory,omitempty"`
+	Subject           string `json:"subject,omitempty"`
+	MessageInstanceId int    `json:"messageInstanceId,omitempty"`
+	Status            int    `json:"status,omitempty"`
+}
+
+type SendMonitor interface {
+	Subscribe(c *websocket.Conn)
+	UnSubscribe(c *websocket.Conn)
+	Pub(msg MonitorData)
+}
+
+type sendMonitor struct {
+	subscribers map[string]*websocket.Conn
+	messageChan chan MonitorData
+	ctx context.Context
+
+	mux sync.Locker
+}
+
+var singleton SendMonitor
+
+func InitMonitor(ctx context.Context) {
+	s := new(sendMonitor)
+	s.ctx = ctx
+	s.messageChan = make(chan MonitorData, 10)
+	s.subscribers = make(map[string]*websocket.Conn)
+	singleton = s
+	go s.pubLoop()
+}
+
+func GetMonitor() SendMonitor {
+	return singleton
+}
+
+func (s *sendMonitor) Subscribe(c *websocket.Conn) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.subscribers[c.ID()] = c
+}
+
+func (s *sendMonitor) UnSubscribe(c *websocket.Conn) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	key := c.ID()
+	if _, ok := s.subscribers[key]; ok {
+		delete(s.subscribers, key)
+	}
+}
+
+func (s *sendMonitor) pubLoop() {
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		case d := <- s.messageChan:
+			s.pub(d)
+		}
+	}
+}
+
+func (s *sendMonitor) pub(msg MonitorData) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	bs, err := json.Marshal(msg)
+	if err != nil {
+		return
+	}
+	for id, c := range s.subscribers {
+		log.Info("task monitor pub to: %s", id)
+		if c.IsClosed() {
+			log.Info("task monitor client[%s] closed skip", id)
+			continue
+		}
+		c.Write(websocket.Message{
+			IsNative: true,
+			Body: bs,
+		})
+	}
+}
+
+func(s *sendMonitor) Pub(msg MonitorData) {
+	s.messageChan <- msg
+}
