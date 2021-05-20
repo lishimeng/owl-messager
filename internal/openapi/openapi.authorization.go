dc921a5b47021efe219ac76a11a948702d737f4c
lishimeng <316527907@qq.com>
2021-05-20 18:35:29 +0800

    auth middware update

---
+++ internal/openapi/openapi.authorization.go
@@ -0,0 +1,23 @@
+package openapi
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/kataras/iris/v12"
+	"github.com/lishimeng/app-starter"
+)
+
+func AuthorizationCk(ctx iris.Context) {
+	_, success := Authorization(ctx)
+	if !success {
+		var resp = app.Response{}
+		resp.Code = 401
+		return
+	}
+	ctx.Next()
+}
+
+
+func Authorization(ctx iris.Context) (claims *jwt.Claims, success bool) {
+
+	return
+}
