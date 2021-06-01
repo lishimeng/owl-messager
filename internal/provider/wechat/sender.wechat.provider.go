fbd14e49789b4c9d3c03632054905a4abc0379ef
lishimeng <316527907@qq.com>
2021-06-01 17:24:12 +0800

    wechat interface

--- internal/provider/wechat/sender.wechat.provider.go
+++ internal/provider/wechat/sender.wechat.provider.go
@@ -8,9 +8,33 @@ type PayloadItem struct {
 	Content string `json:"content"`
 	Color string `json:"color,omitempty"`
 }
+
+type Sender struct {
+	Host string // wechat open platform sender server
+	AppSecret string // app secret
+	AppId string // app id
+}
+
+type ClientCredentialToken struct {
+	AccessToken  string `json:"access_token,omitempty"`
+	ExpireIn     int64  `json:"expire_in,omitempty"`
+	ErrorCode    int64  `json:"errcode,omitempty"`
+	ErrorMessage string `json:"errmsg,omitempty"`
+}
 type To struct {
-	OpenId string // 公众号中的用户唯一ID
-	Uid string // 平台用户ID
-	Payload map[string]PayloadItem // 数据
-	TemplateId string // 公众平台上的模板
+	OpenId string // user's open id of wechat open platform
+	Payload map[string]PayloadItem // data
+	TemplateId string // tpl id on wechat open platform
+}
+
+func (p *Provider) Send(t To) {
+	// TODO send to receiver
+}
+
+func (p *Provider) GetAccessToken() (t ClientCredentialToken) {
+	return
+}
+
+func (p *Provider) TokenExpired() (b bool) {
+	return
 }
