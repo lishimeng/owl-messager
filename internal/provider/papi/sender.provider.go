ea765b3dff522e9dd41713e6d705026d309809fc
lishimeng <316527907@qq.com>
2021-06-11 18:26:03 +0800

    global provider interface

---
+++ internal/provider/papi/sender.provider.go
@@ -0,0 +1,25 @@
+package papi
+
+// MailSender
+// send mail
+type MailSender interface {
+
+}
+
+// SmsSender
+
+type SmsSender interface {
+
+}
+
+// ApnsSender
+
+type ApnsSender interface {
+
+}
+
+// WxSender
+
+type WxSender interface {
+
+}
