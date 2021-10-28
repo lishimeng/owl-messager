8e8521e6caad08be369c0a2e11f332a7378ca422
ryker <1132969953@qq.com>
2021-10-28 16:23:10 +0800

    email_alias

--- internal/db/model/sender.mail.info.go
+++ internal/db/model/sender.mail.info.go
@@ -3,13 +3,14 @@ package model
 // 邮件发送账号
 type MailSenderInfo struct {
 	Pk
-	Code   string `orm:"column(code);unique"`
-	Name   string `orm:"column(name)"`
-	Host   string `orm:"column(host)"`
-	Port   int    `orm:"column(port)"`
-	Email  string `orm:"column(email)"`
-	Alias  string `orm:"column(alias);null"`
-	Passwd string `orm:"column(password)"`
+	Code       string `orm:"column(code);unique"`
+	Name       string `orm:"column(name)"`
+	Host       string `orm:"column(host)"`
+	Port       int    `orm:"column(port)"`
+	Email      string `orm:"column(email)"`
+	EmailAlias string `orm:"column(email_alias)"` // 20211028 新增列 发件人的子邮箱 “from EmailAlias”
+	Alias      string `orm:"column(alias);null"`
+	Passwd     string `orm:"column(password)"`
 
 	TableChangeInfo
 }
