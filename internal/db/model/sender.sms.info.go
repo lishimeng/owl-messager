249726ae4bfcb7a6a7d71cf8256a8ea5e6023187
alex li <alex.dou@qq.com>
2022-03-16 18:30:36 +0800

    增加了短信的配置

--- internal/db/model/sender.sms.info.go
+++ internal/db/model/sender.sms.info.go
@@ -1,1 +1,16 @@
 package model
+
+// SmsSenderInfo 短信发送账号
+type SmsSenderInfo struct {
+	Pk
+	Code       string `orm:"column(code);unique"`
+	Name       string `orm:"column(name)"`
+	Host       string `orm:"column(host)"`
+	Port       int    `orm:"column(port)"`
+	Email      string `orm:"column(email)"`
+	EmailAlias string `orm:"column(email_alias)"` // 20211028 新增列 发件人的子邮箱 “from EmailAlias”
+	Alias      string `orm:"column(alias);null"`
+	Passwd     string `orm:"column(password)"`
+
+	TableChangeInfo
+}
---
+++ internal/db/model/sms.template.model.go
@@ -0,0 +1,18 @@
+package model
+
+type SmsTemplateInfo struct {
+	Pk
+	Code             string `orm:"column(code);unique"`
+	Name             string `orm:"column(name)"`
+	Body             string `orm:"column(body)"`               // 发送的内容主体，可空
+	SenderTemplateId string `orm:"column(sender_template_id)"` // 发送平台的模板ID
+	Signature        string `orm:"column(signature)"`          // 在发送平台上预留的签名，根据不同平台保存签名文本或签名ID
+	Description      string `orm:"column(description);null"`
+	// TODO 参数列表，用来预判断参数是否符合条件
+	TableChangeInfo
+}
+
+const (
+	SmsTemplateEnable  = 1 // enable
+	SmsTemplateDisable = 0 // disable
+)
