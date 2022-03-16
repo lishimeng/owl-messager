fa13f2d763ab108dcf3cdcac5f6ea4f96d84a3da
alex li <alex.dou@qq.com>
2022-03-16 18:47:31 +0800

    短信发送账号配置

--- internal/db/model/sender.sms.info.go
+++ internal/db/model/sender.sms.info.go
@@ -3,14 +3,41 @@ package model
 // SmsSenderInfo 短信发送账号
 type SmsSenderInfo struct {
 	Pk
-	Code       string `orm:"column(code);unique"`
-	Name       string `orm:"column(name)"`
-	Host       string `orm:"column(host)"`
-	Port       int    `orm:"column(port)"`
-	Email      string `orm:"column(email)"`
-	EmailAlias string `orm:"column(email_alias)"` // 20211028 新增列 发件人的子邮箱 “from EmailAlias”
-	Alias      string `orm:"column(alias);null"`
-	Passwd     string `orm:"column(password)"`
+	Code      string    `orm:"column(code);unique"`
+	Name      string    `orm:"column(name)"`
+	Host      string    `orm:"column(host)"`
+	Vendor    SmsVendor `orm:"column(vendor)"` // 消息平台
+	AppKey    string    `orm:"column(app_key);null"`
+	AppSecret string    `orm:"column(app_secret)"`
 
 	TableChangeInfo
+}
+
+type SmsVendor string
+
+const (
+	SmsVendorBaidu   SmsVendor = "baidu_yun"
+	SmsVendorAli     SmsVendor = "ali_yun"
+	SmsVendorHuawei  SmsVendor = "huawei_yun"
+	SmsVendorQiNiu   SmsVendor = "qi_niu_yun"
+	SmsVendorTencent SmsVendor = "tencent_yun"
+	SmsVendorUpYun   SmsVendor = "up_yun"
+)
+
+var SmsVendors map[SmsVendor]byte
+
+func init() {
+	SmsVendors = make(map[SmsVendor]byte)
+	SmsVendors[SmsVendorBaidu] = 0x01
+	SmsVendors[SmsVendorAli] = 0x01
+	SmsVendors[SmsVendorHuawei] = 0x01
+	SmsVendors[SmsVendorQiNiu] = 0x01
+	SmsVendors[SmsVendorTencent] = 0x01
+	SmsVendors[SmsVendorUpYun] = 0x01
+}
+
+// Support 数据库记录的vendor是否被支持
+func (s SmsSenderInfo) Support() bool {
+	val, ok := SmsVendors[s.Vendor]
+	return ok && (val > 0)
 }
