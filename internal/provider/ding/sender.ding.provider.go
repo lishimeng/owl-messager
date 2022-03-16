361f80325bc538b8840c293a59874b2f28c39988
alex li <alex.dou@qq.com>
2022-03-16 10:53:04 +0800

    ding talk 发送robot消息

--- internal/provider/ding/sender.ding.provider.go
+++ internal/provider/ding/sender.ding.provider.go
@@ -1,2 +1,55 @@
 package ding
+
+import (
+	"fmt"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	robot "github.com/alibabacloud-go/dingtalk/robot_1_0"
+)
+
 // 钉钉
+
+type OtoSDK interface {
+}
+
+type otoSdkImpl struct {
+	AppKey    string
+	AppSecret string
+
+	accessToken string
+
+	proxy *robot.Client
+}
+
+func New(appKey, appSecret string) (sdk OtoSDK, err error) {
+
+	var oto otoSdkImpl
+	var client *robot.Client
+	client, err = robot.NewClient(&openapi.Config{
+		AccessKeyId:     &appKey,
+		AccessKeySecret: &appSecret,
+	})
+	if err != nil {
+		return
+	}
+	oto.proxy = client
+
+	return
+}
+
+func (sdk *otoSdkImpl) SendRobotMessage() (err error) {
+	req := robot.SendRobotDingMessageRequest{ // TODO
+		ContentParams:      nil,
+		DingTemplateId:     nil,
+		OpenConversationId: nil,
+		ReceiverUserIdList: nil,
+		RobotCode:          nil,
+	}
+
+	resp, err := sdk.proxy.SendRobotDingMessage(&req)
+
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.String())
+	return
+}
