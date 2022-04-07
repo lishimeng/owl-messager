5d6e1b00bb3e64e391203e64924b5073a08c4743
alex li <alex.dou@qq.com>
2022-04-07 10:55:59 +0800

    sms sender impl

--- internal/messager/sender/sender.sms.go
+++ internal/messager/sender/sender.sms.go
@@ -1,7 +1,71 @@
 package sender
 
-import "github.com/lishimeng/owl/internal/db/model"
+import (
+	"context"
+	"encoding/json"
+	"github.com/lishimeng/go-log"
+	"github.com/lishimeng/owl/internal/db/model"
+	"github.com/lishimeng/owl/internal/db/repo"
+	"github.com/lishimeng/owl/internal/provider/sms"
+	"strings"
+)
 
 type Sms interface {
 	Send(model.SmsMessageInfo) (err error)
+}
+
+type smsSender struct {
+	ctx       context.Context
+	provider  *sms.ProviderManager
+	maxWorker int
+}
+
+func NewSmsSender(ctx context.Context) (m Sms, err error) {
+	m = &smsSender{
+		ctx:       ctx,
+		provider:  sms.New(),
+		maxWorker: 1,
+	}
+
+	return
+}
+
+func (m *smsSender) Send(p model.SmsMessageInfo) (err error) {
+	// sender info
+	log.Info("send sms:%d", p.Id)
+	si, err := repo.GetSmsSenderById(p.Sender)
+	if err != nil {
+		log.Info("sms sender not exist:%d", p.Sender)
+		return
+	}
+
+	tpl, err := repo.GetSmsTemplateById(p.Template)
+	if err != nil {
+		log.Info("sms template not exist:%d", p.Template)
+		return
+	}
+
+	provider := m.provider.Get(si.Vendor)
+
+	var params map[string]interface{}
+	err = json.Unmarshal([]byte(p.Params), &params)
+	if err != nil {
+		log.Info("params is not json format:%s", p.Params)
+		return
+	}
+	toers := strings.Split(p.Receivers, ",")
+	var req = sms.Request{
+		Template:  tpl.SenderTemplateId,
+		Sign:      p.Signature,
+		Params:    params,
+		Receivers: toers,
+	}
+	resp, err := provider.Send(req)
+
+	if err != nil {
+		log.Info(err)
+		return
+	}
+	log.Info(resp)
+	return
 }
