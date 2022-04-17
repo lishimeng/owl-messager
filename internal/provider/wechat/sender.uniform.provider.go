9727e609f6ef31e5ca56c5faba9b5ed3200f7f8c
alex li <alex.dou@qq.com>
2022-04-17 23:50:08 +0800

    ma message send interface

--- internal/provider/wechat/sender.uniform.provider.go
+++ internal/provider/wechat/sender.uniform.provider.go
@@ -2,7 +2,18 @@ package wechat
 
 // 小程序-模板消息
 
+const (
+	uniformHostTpl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/uniform_send?access_token=%s"
+)
+
 type MaProvider struct {
-	AppId     string
-	AppSecret string
+	token string
+}
+
+func (m *MaProvider) ChangeToken() {
+
+}
+
+func (m *MaProvider) Send() {
+
 }
