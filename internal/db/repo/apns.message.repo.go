9a3b888f384931a03a0339ec9c9ce9bcbc8ba551
alex li <alex.dou@qq.com>
2022-04-17 22:48:27 +0800

    create apns message

--- internal/db/repo/apns.message.repo.go
+++ internal/db/repo/apns.message.repo.go
@@ -4,31 +4,26 @@ import (
 	"github.com/lishimeng/app-starter"
 	persistence "github.com/lishimeng/go-orm"
 	"github.com/lishimeng/owl/internal/db/model"
-	"time"
 )
 
-func GetMailByMessageId(msgId int) (m model.MailMessageInfo, err error) {
-	err = app.GetOrm().Context.QueryTable(new(model.MailMessageInfo)).Filter("MessageId", msgId).One(&m)
+func GetApnsByMessageId(msgId int) (m model.ApnsMessageInfo, err error) {
+	err = app.GetOrm().Context.QueryTable(new(model.ApnsSenderInfo)).Filter("MessageId", msgId).One(&m)
 	return
 }
 
-func CreateMailMessage(ctx persistence.TxContext, message model.MessageInfo, sender model.MailSenderInfo, template model.MailTemplateInfo,
-	templateParams string,
-	subject, receiver, cc string) (m model.MailMessageInfo, err error) {
+func CreateApnsMessage(ctx persistence.TxContext, message model.MessageInfo, sender model.ApnsSenderInfo,
+	mode int, bundleId string, params string,
+	subject, receiver string) (m model.ApnsMessageInfo, err error) {
 
 	m.MessageId = message.Id
-	m.Template = template.Id
-	m.Params = templateParams
+	m.ApnsMode = model.ApnsMode(mode)
+	m.BundleId = bundleId
+	m.Params = params
 	m.Sender = sender.Id
 	m.Subject = subject
 	m.Receivers = receiver
-	if len(cc) > 0 {
-		m.Cc = cc
-	}
 
 	m.Status = model.MailTemplateEnable
-	m.CreateTime = time.Now()
-	m.UpdateTime = time.Now()
 
 	_, err = ctx.Context.Insert(&m)
 	return
