b1dd50d2a48ed50cb02796f20560cd43e637e8c4
alex li <alex.dou@qq.com>
2022-04-17 22:47:34 +0800

    apns orm

--- internal/db/model/message.apns.model.go
+++ internal/db/model/message.apns.model.go
@@ -13,9 +13,6 @@ type ApnsMessageInfo struct {
 	Sender int `orm:"column(sender_id)"` // sender's Id
 
 	Receivers string `orm:"column(receiver)"` // receiver list. comma split
-
-	Cc string `orm:"column(cc);null"` // CC
-
 	// 主题
 	Subject string `orm:"column(subject)"`
 }
