f2c65ab78e58c3524e365c8806c7c6ea5eb0f5e2
alex li <alex.dou@qq.com>
2022-04-17 23:58:22 +0800

    uniform request

--- internal/provider/wechat/sender.uniform.provider.go
+++ internal/provider/wechat/sender.uniform.provider.go
@@ -10,6 +10,28 @@ type MaProvider struct {
 	token string
 }
 
+type Req struct {
+	ToUser string   `json:"touser"`
+	MeMsg  MeMsgReq `json:"weapp_template_msg"`
+	MpMsg  MpMsgReq `json:"mp_template_msg"`
+}
+
+type MeMsgReq struct {
+	Tpl     string `json:"template_id"`
+	Page    string `json:"page"`
+	FormId  string `json:"form_id"`
+	Data    string `json:"data"`
+	KeyWord string `json:"emphasis_keyword"`
+}
+
+type MpMsgReq struct {
+	AppId       string `json:"appid"`
+	TemplateId  string `json:"template_id"`
+	Url         string `json:"url"`
+	Data        string `json:"data"`
+	MiniProgram string `json:"miniprogram"`
+}
+
 func (m *MaProvider) ChangeToken() {
 
 }
