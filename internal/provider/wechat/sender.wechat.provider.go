4946920e6e78677dae922b99f9b76347e3227e58
alex li <alex.dou@qq.com>
2022-04-17 23:45:09 +0800

    wx ma apply token

--- internal/provider/wechat/sender.wechat.provider.go
+++ internal/provider/wechat/sender.wechat.provider.go
@@ -1,11 +1,18 @@
 package wechat
 
+import (
+	"fmt"
+	"github.com/lishimeng/go-log"
+)
+
 const (
 	wxHost = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
 )
 
 type Provider struct {
-	Host string // wechat公众号provider地址
+	Host      string // wechat公众号provider地址
+	AppId     string
+	AppSecret string
 }
 
 type PayloadItem struct {
@@ -36,6 +43,8 @@ func (p *Provider) Send(t To) {
 }
 
 func (p *Provider) GetAccessToken() (t ClientCredentialToken) {
+	host := fmt.Sprintf(wxHost, p.AppId, p.AppSecret)
+	log.Info("get wx access token: %s", host)
 	return
 }
 
