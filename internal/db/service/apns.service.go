f3de6240afbe84661bd336d8090d0f586c2ca5ff
alex li <alex.dou@qq.com>
2022-04-17 22:49:11 +0800

    new apns service

--- internal/db/service/apns.service.go
+++ internal/db/service/apns.service.go
@@ -1,12 +1,26 @@
 package service
 
-import "github.com/lishimeng/owl/internal/db/model"
+import (
+	"github.com/lishimeng/app-starter"
+	persistence "github.com/lishimeng/go-orm"
+	"github.com/lishimeng/owl/internal/db/model"
+	"github.com/lishimeng/owl/internal/db/repo"
+	"github.com/lishimeng/owl/internal/messager/msg"
+)
 
-func CreateApnsMessage(sender model.SmsSenderInfo, template model.SmsTemplateInfo, templateParams string,
-	receiver string) (m model.MessageInfo, err error) {
+func CreateApnsMessage(sender model.ApnsSenderInfo,
+	mode int, bundleId string, params string,
+	subject string, receiver string) (m model.MessageInfo, err error) {
 
-	// TODO add apns
-
-	// TODO add Message
+	err = app.GetOrm().Transaction(func(ctx persistence.TxContext) (e error) {
+		// create message
+		m, e = repo.CreateMessage(ctx, subject, msg.Apns)
+		if e != nil {
+			return
+		}
+		// create mail
+		_, _ = repo.CreateApnsMessage(ctx, m, sender, mode, bundleId, params, subject, receiver)
+		return
+	})
 	return
 }
