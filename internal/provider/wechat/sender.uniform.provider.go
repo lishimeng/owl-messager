5d04f6533d5f1dd31b5004a686c93b70f6bf0462
alex li <alex.dou@qq.com>
2022-04-25 00:51:15 +0800

    wechat send message

--- internal/provider/wechat/sender.uniform.provider.go
+++ internal/provider/wechat/sender.uniform.provider.go
@@ -1,12 +1,12 @@
 package wechat
 
-// 小程序-模板消息
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
 
-const ()
-
-type MaProvider struct {
-	token string
-}
+// 小程序-统一消息服务
 
 type Req struct {
 	ToUser string   `json:"touser"`
@@ -30,10 +30,26 @@ type MpMsgReq struct {
 	MiniProgram string `json:"miniprogram"`
 }
 
-func (m *MaProvider) ChangeToken() {
-
+type Resp struct {
+	ErrorCode int    `json:"errcode,omitempty"`
+	ErrorMsg  string `json:"errmsg,omitempty"`
 }
 
-func (m *MaProvider) Send() {
-
+func (p *Provider) Send(req Req) (err error) {
+	var resp Resp
+	err = p.refreshToken()
+	if err != nil {
+		return
+	}
+	host := fmt.Sprintf(uniformHostTpl, p.token.AccessToken)
+	rsp, err := p.rest.R().SetBody(req).Post(host)
+	if err != nil {
+		return
+	}
+	if rsp.StatusCode() != 200 {
+		err = errors.New(rsp.Status())
+		return
+	}
+	err = json.Unmarshal(rsp.Body(), &resp)
+	return
 }
