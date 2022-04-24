058026d615cf662112669f72bdb96cf5bd57e51c
alex li <alex.dou@qq.com>
2022-04-24 20:24:04 +0800

    c

--- internal/provider/wechat/sender.uniform.provider.go
+++ internal/provider/wechat/sender.uniform.provider.go
@@ -2,9 +2,7 @@ package wechat
 
 // 小程序-模板消息
 
-const (
-	uniformHostTpl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/uniform_send?access_token=%s"
-)
+const ()
 
 type MaProvider struct {
 	token string
