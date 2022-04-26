631de6b0aca57f9a4a00567bcd6fab1aed422b6e
alex li <alex.dou@qq.com>
2022-04-27 02:42:42 +0800

    sms provider loader

--- internal/plugins/loader/sms.loader.go
+++ internal/plugins/loader/sms.loader.go
@@ -1,7 +1,14 @@
 package loader
 
+import (
+	"github.com/lishimeng/go-log"
+	"github.com/lishimeng/owl/internal/db/model"
+	"github.com/lishimeng/owl/internal/plugins/container"
+	"github.com/lishimeng/owl/internal/provider/sms"
+)
+
 type SmsLoader interface {
-	Load(id string)
+	Load(id string) sms.Provider
 	Unload(id string)
 }
 
@@ -14,10 +21,21 @@ func New() (sms SmsLoader) {
 type smsLoaderImpl struct {
 }
 
-func (s *smsLoaderImpl) Load(id string) {
-
+func (s *smsLoaderImpl) Load(id string) (p sms.Provider) {
+	var manager, err = container.Get(new(sms.ProviderManager))
+	if err != nil {
+		log.Info(err)
+		return
+	}
+	p = manager.Get(model.SmsVendor(id))
+	return
 }
 
 func (s *smsLoaderImpl) Unload(id string) {
-
+	var _, err = container.Get(new(sms.ProviderManager))
+	if err != nil {
+		log.Info(err)
+		return
+	}
+	// TODO
 }
