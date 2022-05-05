da950bc42c19ff22c81e260ef42a0601d4ddc6b7
alex li <alex.dou@qq.com>
2022-05-05 11:06:42 +0800

    c

--- internal/db/model/orm.models.go
+++ internal/db/model/orm.models.go
@@ -1,1 +1,15 @@
 package model
+
+func Tables() (t []interface{}) {
+	t = append(t,
+		new(MessageInfo),
+		new(MailMessageInfo),
+		new(SmsMessageInfo),
+		new(SmsSenderInfo),
+		new(MailSenderInfo),
+		new(MailTemplateInfo),
+		new(MessageTask),
+		new(MessageRunningTask),
+	)
+	return
+}
