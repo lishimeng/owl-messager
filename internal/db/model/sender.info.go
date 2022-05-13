65827cda811c75bf8dab2a21ce70a86c48a1ebe1
alex li <alex.dou@qq.com>
2022-05-14 00:01:03 +0800

    c

--- internal/db/model/sender.info.go
+++ internal/db/model/sender.info.go
@@ -1,17 +1,9 @@
 package model
 
-// MailSenderInfo 邮件发送账号
-type MailSenderInfo struct {
+// SenderInfo 发消息账号
+type SenderInfo struct {
 	Pk
-	Code       string `orm:"column(code);unique"`
-	Name       string `orm:"column(name)"`
-	Host       string `orm:"column(host)"`
-	Port       int    `orm:"column(port)"`
-	Email      string `orm:"column(email)"`
-	EmailAlias string `orm:"column(email_alias)"` // 20211028 新增列 发件人的子邮箱 “from EmailAlias”
-	Alias      string `orm:"column(alias);null"`
-	Passwd     string `orm:"column(password)"`
-	Default    int    `orm:"column(in_default)"`
-
+	Code    string `orm:"column(code);unique"` // 编号
+	Default int    `orm:"column(in_default)"`  // 默认账号
 	TableChangeInfo
 }
