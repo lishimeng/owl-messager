20e22bbc269180f9fd7f6cb52f17346e84701e0b
alex li <alex.dou@qq.com>
2022-05-15 01:14:35 +0800

    c

--- internal/db/model/orm.models.go
+++ internal/db/model/orm.models.go
@@ -3,13 +3,21 @@ package model
 func Tables() (t []interface{}) {
 	t = append(t,
 		new(MessageInfo),
-		new(MailMessageInfo),
-		new(SmsMessageInfo),
-		new(SmsSenderInfo),
-		new(MailSenderInfo),
-		new(MailTemplateInfo),
 		new(MessageTask),
 		new(MessageRunningTask),
+
+		new(OpenClient),
+
+		new(MailMessageInfo),
+		new(MailTemplateInfo),
+		new(MailSenderInfo),
+
+		new(SmsMessageInfo),
+		new(SmsSenderInfo),
+		new(SmsTemplateInfo),
+
+		new(ApnsMessageInfo),
+		new(ApnsSenderInfo),
 	)
 	return
 }
