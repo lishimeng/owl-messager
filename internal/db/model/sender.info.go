252e0f7dbd4de9cddca44e008d96d988e03a32cd
alex li <alex.dou@qq.com>
2022-05-14 22:40:32 +0800

    sender info

--- internal/db/model/sender.info.go
+++ internal/db/model/sender.info.go
@@ -3,7 +3,12 @@ package model
 // SenderInfo 发消息账号
 type SenderInfo struct {
 	Pk
-	Code    string `orm:"column(code);unique"` // 编号
-	Default int    `orm:"column(in_default)"`  // 默认账号
+	Code    string `orm:"column(code);unique"`    // 编号
+	Default int    `orm:"column(enable_default)"` // 默认账号
 	TableChangeInfo
 }
+
+const (
+	DefaultSenderDisable = 0
+	DefaultSenderEnable  = 1
+)
