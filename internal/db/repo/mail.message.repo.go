4f39673449b3f79f93e99e7ddd8d617d50985606
alex li <alex.dou@qq.com>
2022-05-15 11:33:51 +0800

    mail sender deprecate

--- internal/db/repo/mail.message.repo.go
+++ internal/db/repo/mail.message.repo.go
@@ -12,14 +12,16 @@ func GetMailByMessageId(msgId int) (m model.MailMessageInfo, err error) {
 	return
 }
 
-func CreateMailMessage(ctx persistence.TxContext, message model.MessageInfo, sender model.MailSenderInfo, template model.MailTemplateInfo,
+func CreateMailMessage(ctx persistence.TxContext, message model.MessageInfo, sender *model.MailSenderInfo, template model.MailTemplateInfo,
 	templateParams string,
 	subject, receiver, cc string) (m model.MailMessageInfo, err error) {
 
 	m.MessageId = message.Id
 	m.Template = template.Id
 	m.Params = templateParams
-	m.Sender = sender.Id
+	if sender != nil {
+		m.Sender = sender.Id
+	}
 	m.Subject = subject
 	m.Receivers = receiver
 	if len(cc) > 0 {
