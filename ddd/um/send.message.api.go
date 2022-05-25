56ec026eff7b48de336ea0c4bad89626eaf62929
alex li <alex.dou@qq.com>
2022-05-25 16:08:00 +0800

    ddd#send union message interface

---
+++ ddd/um/send.message.api.go
@@ -0,0 +1,23 @@
+package um
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/lishimeng/app-starter"
+	"github.com/lishimeng/go-log"
+)
+
+type Req struct {
+	Template      string      `json:"template"` // 模板
+	TemplateParam interface{} `json:"params"`   // 参数
+	Receiver      string      `json:"receiver"` // 接收者，多个时用逗号分隔
+	Category      int         `json:"category"` // 消息类型
+}
+
+type Resp struct {
+	app.Response
+	MessageId int `json:"messageId,omitempty"`
+}
+
+func sendMessage(ctx iris.Context) {
+	log.Info("Union message send function")
+}
