50161ddb23ab01367accf3f006fbe8b1b7734a83
alex li <alex.dou@qq.com>
2022-05-25 16:08:55 +0800

    ddd router

--- ddd/router.go
+++ ddd/router.go
@@ -1,7 +1,11 @@
 package ddd
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/lishimeng/owl/ddd/um"
+)
 
 func Router(root iris.Party) {
 
+	um.Route(root.Party("/union_message"))
 }
