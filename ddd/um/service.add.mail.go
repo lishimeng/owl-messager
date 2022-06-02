ea36b13c7f1d39ad180a77fc583b56a99d62fec0
alex li <alex.dou@qq.com>
2022-06-02 18:02:21 +0800

    um add mail service

--- ddd/um/service.add.mail.go
+++ ddd/um/service.add.mail.go
@@ -1,5 +1,20 @@
 package um
 
-func addMailService() {
+import (
+	"github.com/lishimeng/owl/internal/db/model"
+	"github.com/lishimeng/owl/internal/db/repo"
+	"github.com/lishimeng/owl/internal/db/service"
+)
 
+func addMailService(templateCode, params, subject, receiver string) (m model.MessageInfo, err error) {
+	tpl, err := repo.GetMailTemplateByCode(templateCode)
+	if err != nil {
+		return
+	}
+	m, err = service.CreateMailMessage(
+		nil,
+		tpl,
+		params,
+		subject, receiver, "")
+	return
 }
