664fec67a8b6efeef099df4d6fb0b76b658111d6
alex li <alex.dou@qq.com>
2022-06-14 10:19:21 +0800

    sender list api(ddd)

--- ddd/sender/api.go
+++ ddd/sender/api.go
@@ -1,9 +1,27 @@
 package sender
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/lishimeng/app-starter"
+	"github.com/lishimeng/owl/internal/api/common"
+)
+
+type Item struct {
+}
 
 func list(ctx iris.Context) {
 
+	var resp app.PagerResponse
+
+	var items []Item
+	// TODO
+
+	for _, item := range items {
+		resp.Data = append(resp.Data, item)
+	}
+	// TODO pager
+	resp.Code = common.RespCodeSuccess
+	common.ResponseJSON(ctx, resp)
 }
 
 func mailSenderInfo(ctx iris.Context) {
