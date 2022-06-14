f63f231b82cec1a5e07dcde13c508538849961af
alex li <alex.dou@qq.com>
2022-06-14 10:08:20 +0800

    register new module(sender[ddd])

--- ddd/router.go
+++ ddd/router.go
@@ -2,10 +2,12 @@ package ddd
 
 import (
 	"github.com/kataras/iris/v12"
+	"github.com/lishimeng/owl/ddd/sender"
 	"github.com/lishimeng/owl/ddd/um"
 )
 
 func Router(root iris.Party) {
 
 	um.Route(root.Party("/union_message"))
+	sender.Route(root.Party("/sender"))
 }
--- ddd/sender/route.go
+++ ddd/sender/route.go
@@ -1,1 +1,7 @@
 package sender
+
+import "github.com/kataras/iris/v12"
+
+func Route(root iris.Party) {
+
+}
