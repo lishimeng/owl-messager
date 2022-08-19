77b358b318ffd4d146623d12054f7aa891002070
alex li <alex.dou@qq.com>
2022-08-19 17:45:57 +0800

    graph provider初始化

--- internal/provider/mail/ms/provider.new.go
+++ internal/provider/mail/ms/provider.new.go
@@ -4,22 +4,28 @@ import (
 	"crypto"
 	"crypto/x509"
 	"errors"
+	"github.com/lishimeng/owl/internal/certificate"
 )
 
 const (
-	clientIdKey  = "clientId"
-	tenantKey    = "tenant"
-	scopeKey     = "scope"
-	certKey      = "certificate"
-	certKetKey   = "certificateKey"
-	certCategory = "certCategory"
-	certPassword = "certPassword"
+	clientIdKey    = "clientId"
+	tenantKey      = "tenant"
+	scopeKey       = "scope"
+	certificateKey = "certificate"
+	privateKey     = "certificateKey"
+	senderKey      = "sender"
 )
 
 func New(config map[string]string) (p *AzureGraphProvider, err error) {
 
-	// TODO build provider
 	var c AzureAuthConfig
+
+	if sender, ok := config[senderKey]; ok && len(sender) > 0 {
+		c.Sender = sender
+	} else {
+		err = errors.New("sender nil")
+		return
+	}
 
 	if tenant, ok := config[tenantKey]; ok && len(tenant) > 0 {
 		c.Tenant = tenant
@@ -42,19 +48,28 @@ func New(config map[string]string) (p *AzureGraphProvider, err error) {
 		return
 	}
 
-	if certPem, ok := config[certKey]; ok && len(certPem) > 0 {
+	if certPem, ok := config[certificateKey]; ok && len(certPem) > 0 {
 
-		var cert x509.Certificate
-		// TODO
-		c.Certificate = append(c.Certificate, &cert)
+		var crt *x509.Certificate
+		h := certificate.PemHandler{Pem: certPem}
+		crt, err = h.ParseCrt()
+		if err != nil {
+			return
+		}
+		c.Certificate = append(c.Certificate, crt)
 	} else {
 		err = errors.New("cert nil")
 		return
 	}
 
-	if certKeyPem, ok := config[certKetKey]; ok && len(certKeyPem) > 0 {
+	if certKeyPem, ok := config[privateKey]; ok && len(certKeyPem) > 0 {
 
 		var certKey crypto.PrivateKey
+		h := certificate.PemHandler{Pem: certKeyPem}
+		certKey, err = h.ParseKey()
+		if err != nil {
+			return
+		}
 		c.CertificateKey = certKey
 	} else {
 		err = errors.New("tenant nil")
