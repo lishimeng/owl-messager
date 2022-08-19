9ca7fabfcfab718aa39c7c1641040cc172b6bf06
alex li <alex.dou@qq.com>
2022-08-19 17:44:24 +0800

    graph请求数据

--- internal/provider/mail/ms/graph.model.go
+++ internal/provider/mail/ms/graph.model.go
@@ -76,7 +76,7 @@ func BuildMessage(subject string, body string, receivers ...string) (m MessageWr
 	m.Message = Message{
 		Subject: subject,
 		Body: MessageBody{
-			ContentType: "text/html",
+			ContentType: "Text",
 			Content:     body,
 		},
 	}
