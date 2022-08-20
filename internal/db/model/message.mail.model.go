358d358fe140fb1c80b6a69d6a99e8fc7b76e3d6
alex li <alex.dou@qq.com>
2022-08-20 23:01:20 +0800

    c

--- internal/db/model/message.mail.model.go
+++ internal/db/model/message.mail.model.go
@@ -1,19 +1,10 @@
 package model
 
-// 邮件
+// MailMessageInfo 邮件
 type MailMessageInfo struct {
 	MessageHeader
-
-	Template int `orm:"column(template_id)"` // mail template
-
-	Params string `orm:"column(params);null"` // json params
-
-	Sender int `orm:"column(sender_id);null"` // sender's Id
-
-	Receivers string `orm:"column(receiver)"` // receiver list. comma split
-
-	Cc string `orm:"column(cc);null"` // CC
-
-	// 主题
-	Subject string `orm:"column(subject)"`
+	Template  int    `orm:"column(template_id)"` // 模板
+	Params    string `orm:"column(params);null"` // 参数
+	Receivers string `orm:"column(receiver)"`    // 收件人，多个情况下用逗号分隔
+	Subject   string `orm:"column(subject)"`     // 标题
 }
