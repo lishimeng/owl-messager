fa682115ebff37021092dc7645faae7cca72379f
alex li <alex.dou@qq.com>
2022-08-21 10:00:07 +0800

    c

--- internal/setup/app.after.go
+++ internal/setup/app.after.go
@@ -5,11 +5,6 @@ import (
 )
 
 func AfterStarted(ctx context.Context) (err error) {
-	err = loadSmsProviders(ctx)
-	if err != nil {
-		return
-	}
-
 	err = messageSendProcess(ctx)
 	if err != nil {
 		return
