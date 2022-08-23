92385610e2fcd3fb1c89635bcdd5361aa1192eb1
alex li <alex.dou@qq.com>
2022-08-23 13:51:07 +0800

    build_script

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,27 @@
 FROM node:18.4.0 as ui
+ARG NAME
+ARG VERSION
 WORKDIR /ui_build
 ADD ui .
 RUN npm install && npm run build
 
 FROM golang:1.18 as build
-ARG APP_VERSION
+ARG NAME
+ARG VERSION
+ARG COMMIT
+ARG TAG
+ARG BUILD_TIME
+ARG MAIN_PATH
 ENV GOPROXY=https://goproxy.cn,direct
 WORKDIR /release
 ADD . .
-
-COPY --from=ui /ui_build/dist /release/static/
-
 RUN go mod download && go mod verify
-
-RUN go build -v --ldflags "-X cmd.Version=${APP_VERSION}" -o owl-messager cmd/owl-messager/main.go
-
+RUN go build -v --ldflags "-X cmd.AppName=${NAME} -X cmd.Version=${VERSION} -X cmd.Commit=${COMMIT} -X cmd.Build=${BUILD_TIME}" -o ${NAME} ${MAIN_PATH}
 
 FROM ubuntu:22.04 as prod
+ARG NAME
 EXPOSE 80/tcp
 WORKDIR /
-COPY --from=build /release/owl-messager /
-ENTRYPOINT ["/owl-messager"]
+COPY --from=build /release/${NAME} /
+RUN ln -s /${NAME} /app
+CMD [ "/app"]
--- build.sh
+++ build.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-APP_NAME="owl-messager"
+NAME="owl-messager"
+MAIN_PATH="cmd/owl-messager/main.go"
+ORG="lishimeng"
 
 # shellcheck disable=SC2046
-TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+# shellcheck disable=SC2154
+COMMIT=$(git log --pretty=format:"%h" -1)
+BUILD_TIME=$(date +%FT%T%z)
 
 build_application(){
-  git checkout "${TAG}"
-  docker build -t lishimeng/owl-messager:"$TAG" --build-arg APP_VERSION="${TAG}" .
+  git checkout "${VERSION}"
+  docker build -t "${ORG}/${NAME}:${VERSION}" \
+  --build-arg NAME="${NAME}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg COMMIT="${COMMIT}" \
+  --build-arg BUILD_TIME="${BUILD_TIME}" \
+  --build-arg MAIN_PATH="${MAIN_PATH}" .
 }
 
-help_print(){
-  echo "build ${APP_NAME}"
-  echo "Version:$TAG"
+print_app_info(){
+  echo "****************************************"
+  echo "App:${NAME}"
+  echo "Version:${VERSION}"
+  echo "Commit:${COMMIT}"
+  echo "BuildTime:${BUILD_TIME}"
+  echo "Main_Path:${MAIN_PATH}"
+  echo "****************************************"
+  echo ""
 }
 
-help_print
+print_app_info
 build_application
