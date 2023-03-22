6fe5098a3d386ba26e7d221a18dac2d730910267
alex.dou <alex.dou@qq.com>
2023-03-22 22:58:04 +0800

    tencent client

--- internal/provider/mail/tencent/tencent.go
+++ internal/provider/mail/tencent/tencent.go
@@ -1,1 +1,14 @@
 package tencent
+
+import (
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/regions"
+	ses "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/ses/v20201002"
+)
+
+func Init(appId, secret string) (client *ses.Client, err error) {
+	credential := common.NewCredential(appId, secret)
+	client, err = ses.NewClient(credential, regions.Shanghai, profile.NewClientProfile())
+	return
+}
