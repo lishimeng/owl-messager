ede629be8ffe79f9a91cc46f94a1bed22b4a11b7
alex.dou <alex.dou@qq.com>
2023-03-26 01:52:41 +0800

    c

--- internal/util/string.go
+++ internal/util/string.go
@@ -5,3 +5,14 @@ import "strings"
 func Join(j string, s ...string) string {
 	return strings.Join(s, j)
 }
+
+// StatusIn 判定状态是否在列表中
+func StatusIn(status int, list []int) (b bool) {
+	for _, v := range list {
+		if status == v {
+			b = true
+			break
+		}
+	}
+	return
+}
