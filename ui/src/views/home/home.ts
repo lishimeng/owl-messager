695077ae1370cfb4777c8ec38f63433a3cb81efd
alex.dou <alex.dou@qq.com>
2023-04-05 18:00:57 +0800

    c

---
+++ ui/src/views/home/home.ts
@@ -0,0 +1,20 @@
+class HomeOne {
+
+    public num1: string = ""
+    public num2: string = ""
+    public num3: string = ""
+    public num4: string = ""
+    public color1: string = ""
+    public color2: string = ""
+    public color3: string = ""
+}
+
+class HomeThree {
+
+    icon: string = ""
+    label: string = ""
+    value: string = ""
+    iconColor: string = ""
+}
+
+export { HomeOne, HomeThree }
