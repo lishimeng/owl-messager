47959bde3c642af10e57091420dd737f69423424
alex.dou <alex.dou@qq.com>
2023-04-06 00:53:00 +0800

    c

--- ui/src/layout/footer/index.vue
+++ ui/src/layout/footer/index.vue
@@ -1,14 +1,21 @@
 <template>
 	<div class="layout-footer pb15">
 		<div class="layout-footer-warp">
-			<div>vue-next-admin，Made by lyt with ❤️</div>
-			<div class="mt5">深圳市 xxx 公司版权所有</div>
+			<div>{{ state.description }}</div>
+			<div class="mt5">{{ state.copyRight }}</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts" name="layoutFooter">
 // 此处需有内容（注释也得），否则缓存将失败
+// 定义变量内容
+import {reactive} from "vue";
+
+const state = reactive({
+	description: 'Owl Messager, message push provider',
+	copyRight: 'githug.com/lishimeng版权所有'
+})
 </script>
 
 <style scoped lang="scss">
