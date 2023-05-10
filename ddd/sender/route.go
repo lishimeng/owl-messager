75a1d09ce8fe441af96d3a00ebff87fb5261774d
alex.dou <alex.dou@qq.com>
2023-05-10 16:31:34 +0800

    c

--- ddd/sender/route.go
+++ ddd/sender/route.go
@@ -4,10 +4,10 @@ import "github.com/kataras/iris/v12"
 
 func Route(root iris.Party) {
 
-	root.Get("/", list)
+	root.Get("/", list) // 列表
 	root.Get("/mail/{id}", mailSenderInfo)
 	root.Get("/sms/{id}", smsSenderInfo)
 	root.Get("/apns/{id}", apnsSenderInfo)
-	root.Post("/{org}/{category}/{id:int}/set_default", setDefaultSender)
-	root.Get("/config/{category}/{vendor}", getConfigStruct)
+	root.Post("/{org}/{category}/{id:int}/set_default", setDefaultSender) // 设置组织下默认发信账号
+	root.Get("/config/{category}/{vendor}", getConfigStruct)              // 列出配置字段
 }
