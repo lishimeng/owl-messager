0481df9dee74339248cac7a39b20ee25ec49d5b1
alex.dou <alex.dou@qq.com>
2023-05-15 15:25:58 +0800

    tmp

--- ddd/sender/service.go
+++ ddd/sender/service.go
@@ -1,1 +1,14 @@
 package sender
+
+import (
+	"github.com/lishimeng/app-starter"
+	"github.com/lishimeng/owl/internal/db/model"
+)
+
+func svsGetSenders(org int) (senders []model.SenderInfo, err error) {
+
+	_, err = app.GetOrm().Context.
+		QueryTable(new(model.SenderInfo)).
+		Filter("").All(&senders)
+	return
+}
