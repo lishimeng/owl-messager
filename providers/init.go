626bb5a4f97e47e9271e230502001aee2d15192c
alex.dou <alex.dou@qq.com>
2023-05-23 14:45:11 +0800

    c

--- providers/init.go
+++ providers/init.go
@@ -1,10 +1,12 @@
 package providers
 
 import (
+	"encoding/json"
 	"github.com/lishimeng/owl/internal/db/model"
 	"github.com/lishimeng/owl/internal/messager"
 	"github.com/lishimeng/owl/internal/provider"
 	"github.com/lishimeng/owl/providers/mail"
+	"github.com/lishimeng/owl/providers/sms"
 )
 
 func registerMailProviders() {
@@ -21,6 +23,39 @@ func registerMailProviders() {
 	})
 }
 
+func registerSmsProviderBuilders() {
+
+	provider.RegisterSmsProvider(model.SmsVendorAli, func(config string) (p messager.SmsProvider, err error) {
+		var aliSmsConf model.AliSmsConfig
+		h := sms.AliProvider{}
+		err = json.Unmarshal([]byte(config), &aliSmsConf)
+		if err != nil {
+			return
+		}
+		err = h.Init(aliSmsConf)
+		if err != nil {
+			return
+		}
+		p = &h
+		return
+	})
+
+	provider.RegisterSmsProvider(model.SmsVendorTencent, func(config string) (p messager.SmsProvider, err error) {
+		var tencentSmsConf model.TencentSmsConfig
+		err = json.Unmarshal([]byte(config), &tencentSmsConf)
+		if err != nil {
+			return
+		}
+		h, err := sms.NewTencent(tencentSmsConf)
+		if err != nil {
+			return
+		}
+		p = h
+		return
+	})
+}
+
 func init() {
 	registerMailProviders()
+	registerSmsProviderBuilders()
 }
