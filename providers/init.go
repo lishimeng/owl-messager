07b38b395f2b18a2c3b65ba5510c4279b0b16c7d
alex.dou <alex.dou@qq.com>
2023-05-23 14:22:10 +0800

    register mail providers

--- providers/init.go
+++ providers/init.go
@@ -1,1 +1,26 @@
 package providers
+
+import (
+	"github.com/lishimeng/owl/internal/db/model"
+	"github.com/lishimeng/owl/internal/messager"
+	"github.com/lishimeng/owl/internal/provider"
+	"github.com/lishimeng/owl/providers/mail"
+)
+
+func registerMailProviders() {
+	provider.RegisterMailProvider(model.MailVendorSmtp, func(config string) (messager.MailProvider, error) {
+		return mail.NewSmtp(config)
+	})
+
+	provider.RegisterMailProvider(model.MailVendorMicrosoft, func(config string) (messager.MailProvider, error) {
+		return mail.NewMicrosoft(config)
+	})
+
+	provider.RegisterMailProvider(model.MailVendorTencent, func(config string) (messager.MailProvider, error) {
+		return mail.NewTencent(config)
+	})
+}
+
+func init() {
+	registerMailProviders()
+}
