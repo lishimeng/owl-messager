464f6d5cc0aec35e890611e3356ad459722277af
alex.dou <alex.dou@qq.com>
2023-06-29 16:10:03 +0800

    client option

--- sdk/client.impl.go
+++ sdk/client.impl.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/lishimeng/go-log"
 	"github.com/lishimeng/owl-messager/internal/messager/msg"
+	"github.com/pkg/errors"
 	"io"
 	"net/http"
 	"net/url"
@@ -15,28 +16,42 @@ const ApiPath = "/v2/messages/"
 
 // MessageClient 消息服务
 type MessageClient struct {
-	Host     string // 消息服务主机地址. 如, "http://127.0.0.1/api"
-	category string // 消息类型. "mail","sms","apns"
+	host        string // 消息服务主机地址. 如, "http://127.0.0.1/api"
+	debugEnable bool
 }
 
-// NewClient 初始化邮件服务信息。host: 邮件服务主机地址
-func NewClient(host string) (m *MessageClient) {
-	return &MessageClient{
-		Host: host,
+type Option func(*MessageClient)
+
+func WithHost(host string) Option {
+	return func(client *MessageClient) {
+		client.host = host
 	}
 }
 
-func (m *MessageClient) setCategory(category string) {
-	m.category = category
+func WithDebug(enable bool) Option {
+	return func(client *MessageClient) {
+		client.debugEnable = enable
+	}
+}
+
+// NewClient 初始化邮件服务信息。host: 邮件服务主机地址
+func NewClient(options ...Option) (m *MessageClient) {
+	m = &MessageClient{}
+	for _, opt := range options {
+		opt(m)
+	}
+	return
 }
 
 func (m *MessageClient) getURL(category string) (address string, err error) {
-	address, err = url.JoinPath(m.Host, ApiPath, category)
+	address, err = url.JoinPath(m.host, ApiPath, category)
 	return
 }
 
 func (m *MessageClient) SendMail(request MailRequest) (response Response, err error) {
-	log.Debug("sendMail to: %s", request.Receiver)
+	if m.debugEnable {
+		log.Debug("sendMail to: %s", request.Receiver)
+	}
 	response, err = m.send(msg.EmailCategory, request)
 	if err != nil {
 		log.Debug(err)
@@ -45,7 +60,9 @@ func (m *MessageClient) SendMail(request MailRequest) (response Response, err er
 	return
 }
 func (m *MessageClient) SendSms(request SmsRequest) (response Response, err error) {
-	log.Debug("sendMail to: %s", request.Receiver)
+	if m.debugEnable {
+		log.Debug("sendMail to: %s", request.Receiver)
+	}
 	response, err = m.send(msg.SmsCategory, request)
 	if err != nil {
 		log.Debug(err)
@@ -54,7 +71,9 @@ func (m *MessageClient) SendSms(request SmsRequest) (response Response, err erro
 	return
 }
 func (m *MessageClient) SendApns(request ApnsRequest) (response Response, err error) {
-	log.Debug("sendMail to: %s", request.Receiver)
+	if m.debugEnable {
+		log.Debug("sendMail to: %s", request.Receiver)
+	}
 	response, err = m.send(msg.ApnsCategory, request)
 	if err != nil {
 		log.Debug(err)
@@ -66,14 +85,17 @@ func (m *MessageClient) SendApns(request ApnsRequest) (response Response, err er
 func (m *MessageClient) send(category string, request interface{}) (response Response, err error) {
 	u, err := m.getURL(category)
 	if err != nil {
+		log.Debug(errors.Wrap(err, "get url fail"))
 		return
 	}
-	log.Debug("sendMail url: %s", u)
+	if m.debugEnable {
+		log.Debug("sendMail url: %s", u)
+	}
 	client := &http.Client{Timeout: 8 * time.Second}
 	jsonStr, _ := json.Marshal(request)
 	resp, err := client.Post(u, "application/json", bytes.NewBuffer(jsonStr))
 	if err != nil {
-		log.Debug("client Post err")
+		log.Debug(errors.Wrap(err, "client Post err"))
 		return
 	}
 	defer func(Body io.ReadCloser) {
@@ -83,9 +105,11 @@ func (m *MessageClient) send(category string, request interface{}) (response Res
 	result, _ := io.ReadAll(resp.Body)
 	err = json.Unmarshal(result, &response)
 	if err != nil {
-		log.Debug("response Unmarshal err, %+v", response)
+		log.Debug(errors.Wrap(err, "response json unmarshal err"))
 		return
 	}
-	log.Debug("sendMail response: %v", response)
+	if m.debugEnable {
+		log.Debug("sendMail response: %v", response)
+	}
 	return
 }
