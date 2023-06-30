6ecc0e10897d28c5a79b85c24b8845605d1cdd31
alex.dou <alex.dou@qq.com>
2023-07-01 04:27:20 +0800

    推送latest tag镜像

--- build.sh
+++ build.sh
@@ -43,6 +43,8 @@ push_image(){
   echo "Push:${Org}:${Name}:${Version}"
   echo "****************************************"
   echo ""
+  docker tag  "${Org}/${Name}:${Version}" "${Org}/${Name}"
+  docker push "${Org}/${Name}"
   docker push "${Org}/${Name}:${Version}"
 }
 
