38c87d7b9446b2b77c13ff72aa9b04a1c2bb1231
alex.dou <alex.dou@qq.com>
2023-07-19 10:45:35 +0800

    sdk 发送邮件不再需要标记cloud template

--- sdk/request.go
+++ sdk/request.go
@@ -2,11 +2,11 @@ package sdk
 
 // MailRequest 邮件服务请求参数
 type MailRequest struct {
-	Template      string      `json:"template"`                // 模板
-	CloudTemplate bool        `json:"cloudTemplate,omitempty"` // 云端模板
-	TemplateParam interface{} `json:"params"`                  // 参数
-	Title         string      `json:"subject,omitempty"`       // 标题
-	Receiver      string      `json:"receiver"`                // 接收者，多个时用逗号分隔
+	Template string `json:"template"` // 模板
+	//CloudTemplate bool        `json:"cloudTemplate,omitempty"` // 云端模板(不需要)
+	TemplateParam interface{} `json:"params"`            // 参数
+	Title         string      `json:"subject,omitempty"` // 标题
+	Receiver      string      `json:"receiver"`          // 接收者，多个时用逗号分隔
 }
 
 // SmsRequest 短信服务请求参数
