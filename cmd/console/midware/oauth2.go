e20f74b9ad894b16c292b8aa0a228a260f9c0178
alex.dou <alex.dou@qq.com>
2023-07-20 14:22:39 +0800

    update app starter

---
+++ cmd/console/midware/oauth2.go
@@ -0,0 +1,23 @@
+package midware
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/lishimeng/app-starter/midware/auth"
+	"github.com/lishimeng/owl-messager/internal/common"
+	"github.com/lishimeng/owl-messager/internal/etc"
+)
+
+// WithAuth token验证器,
+// auth.JwtBasic header预处理
+// auth.Forbidden401Handler 无权限时返回401, 返回格式按照参数 auth.ForbiddenOption
+func WithAuth(handler func(iris.Context)) []iris.Handler {
+	var handlers []iris.Handler
+	if etc.Config.Token.Enable {
+		handlers = append(handlers,
+			auth.JwtBasic(),
+			auth.Forbidden401Handler(auth.WithJsonResp(), auth.WithScope(common.Scope)),
+		)
+	}
+	handlers = append(handlers, handler)
+	return handlers
+}
