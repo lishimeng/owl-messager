aefacd2358e69e34afe23edf03a842b1a91548d5
alex.dou <alex.dou@qq.com>
2024-04-12 20:06:19 +0800

    数据字典api, 增加provider列表

--- cmd/console/ddd/dict/providers.list.api.go
+++ cmd/console/ddd/dict/providers.list.api.go
@@ -1,1 +1,55 @@
 package dict
+
+import "C"
+import (
+	"github.com/baidubce/bce-sdk-go/util/log"
+	"github.com/lishimeng/app-starter"
+	"github.com/lishimeng/app-starter/server"
+	"github.com/lishimeng/app-starter/tool"
+	"github.com/lishimeng/owl-messager/internal/provider"
+	"github.com/lishimeng/owl-messager/pkg/msg"
+)
+
+type Providers struct {
+	app.Response
+	Items []ProviderInfo `json:"items,omitempty"`
+}
+
+type ProviderInfo struct {
+	Name     string `json:"name,omitempty"`
+	Category string `json:"category,omitempty"`
+}
+
+// 支持的provider列表
+func providerList(ctx server.Context) {
+	log.Info("get all providers...")
+	var resp Providers
+	// 返回支持的provider列表
+
+	// Mail providers
+	mailList := provider.GetMailProviders()
+	for _, mail := range mailList {
+		resp.Items = append(resp.Items, ProviderInfo{
+			Name:     mail.String(),
+			Category: string(msg.MailMessage),
+		})
+	}
+
+	// Sms providers
+	smsList := provider.GetSmsProviders()
+	for _, mail := range smsList {
+		resp.Items = append(resp.Items, ProviderInfo{
+			Name:     mail.String(),
+			Category: string(msg.SmsMessage),
+		})
+	}
+
+	resp.Code = tool.RespCodeSuccess
+	ctx.Json(resp)
+}
+
+func providerCategoryList(ctx server.Context) {
+	// 返回支持的provider列表
+	var category = ctx.C.Params().Get("category")
+	log.Info(category)
+}
--- cmd/console/ddd/dict/router.go
+++ cmd/console/ddd/dict/router.go
@@ -1,1 +1,9 @@
 package dict
+
+import "github.com/lishimeng/app-starter/server"
+
+func Router(r server.Router) {
+	r.Get("/providers", providerList)
+	r.Get("/providers/{category}", providerList)
+	r.Get("/provider/{id}", providerList)
+}
